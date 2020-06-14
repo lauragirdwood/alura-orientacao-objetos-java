2cb3e69bb0d3f09e15a2113a9252b1d2a60d8092
Laura <azevedoemello@gmail.com>
2020-06-14 00:01:28 -0300

    Criando projeto e adicionando classe Conta e CriaConta

---
+++ src/com/company/Conta.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Conta {
+
+    String titular;
+    int agencia;
+    int numero;
+    int DAC;
+    double saldo;
+
+}
---
+++ src/com/company/CriaConta.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class CriaConta {
+
+    public static void main(String[] args) {
+	    Conta primeiraConta = new Conta();
+	    primeiraConta.saldo = 200.00d;
+    }
+}
